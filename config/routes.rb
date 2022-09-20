ff70791b5b5580d3420632972f75062785b0c2bd
Student astrum <student@astrum.domain>
2022-09-20 13:24:57 +0500

    MY_basecamp_2

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'users/profile'
+  devise_for :users
+  get '/u/:id', to: 'users#profile', as: 'user'
+  # /posts/1/comments/4
+  resources :posts do 
+    resources :comments
+  end
+  get 'about', to: 'pages#about'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+   root "pages#home"
+end
